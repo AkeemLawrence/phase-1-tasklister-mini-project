288bc72eeda401da8b27580c763bece9f3e5179e
Akeem Lawrence <akeemwlawrence@gmail.com>
2023-12-20 20:06:24 -0500

    commit tree a8ae0df6c234baf8975e6fabb0d6dd322929fecd

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+//As a user, I should be able to type a task into the input field.
+//Get input
+//Change input
+//Prevent default
+//Change DOM
+
+//As a user, I should be able to click some form of a submit button.
+//As a user, I expect to see the task string that I provided appear in the DOM after the submit button has been activated.
+
